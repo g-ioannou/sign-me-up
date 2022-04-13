94e0652873cd052165a5c53485784cb776e0dd55
BrainlessPOMO <agg2elos@gmail.com>
2022-04-13 23:13:50 +0300

    Basic GUI on login/sign form

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,95 @@
-import { Button } from "react-bootstrap";
-import { ButtonGroup } from "react-bootstrap";
-import { ToggleButton } from "react-bootstrap";
-import { Form } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Button,
+  ButtonGroup,
+  Container,
+  Row,
+  Col,
+  Image,
+} from "react-bootstrap";
+
 import React from "react";
-import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+import LoginForm from "./components/loginForm";
+import SignupForm from "./components/signupForm";
 
 function App() {
+  //for login & signup buttons
   const [checked, setChecked] = React.useState(false);
-  const [radioValue, setRadioValue] = React.useState('1');
+  const [radioValue, setRadioValue] = React.useState("1");
 
   const radios = [
-    { name: 'Login', value: '1' },
-    { name: 'SignUp', value: '2' },
+    { name: "Login", value: "1", login: true, signup: false },
+    { name: "SignUp", value: "2", login: false, signup: true },
   ];
-  return (
-    <div className="fullBackground" style={{ 
-      backgroundImage: `url(${process.env.PUBLIC_URL + 'images/map_background.png'})`, height: '100vh'
-    }}>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 left-homescreen">
-            <div className="login-form"> 
-                <ButtonGroup>
-                  {radios.map((radio, idx) => (
-                    <Button
-                      key={idx}
-                      id={`radio-${idx}`}
-                      type="radio"
-                      variant="custom"
-                      value={radio.value}
-                      checked={radioValue === radio.value}
-                      onChange={(e) => setRadioValue(e.currentTarget.value)}
-                    >
-                      {radio.name}
-                    </Button>
-                  ))}
-                </ButtonGroup>
-              <div className="login-body">
-                  <Form>
-                    <Form.Group className="mb-3" controlId="loginFormEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="loginFormPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Button variant="custom" type="submit">
-                      Submit
-                    </Button>
-                  </Form>
-              </div>
-              <div className="signup-body">
-                <Form>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                  </Form.Group>
-                
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                  </Form.Group>
-                </Form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  //styles
+  const styles = {
+    formContainer: {
+      margin: "2em",
+      padding: "2em",
+    },
+    leftBar: {
+      height: window.innerHeight,
+      overflow: "hidden",
+      backgroundColor: "rgba(255, 255, 255, 0.6)",
+      padding: "5em",
+    },
+    image: {
+      marginTop: "2em",
+      marginBottom: "10em",
+      width: "100%",
+    },
+  };
+
+  //for oppening the forms
+  const [loginForm, setLoginForm] = React.useState(true);
+  return (
+    <>
+      {/* <div
+       className="fullBackground"
+       style={{
+         backgroundImage: `url(${
+           process.env.PUBLIC_URL + "images/map_background.png"
+         })`,
+         height: "100vh",
+       }}
+     >
+       */}
+      <Image
+        src="images/map_background.png"
+        className="img-fluid "
+        style={{ minHeight: "100vh", zIndex: "-1", position: "absolute" }}
+      />
+      <Container fluid>
+        <Row>
+          <Col xs={12} md={6} lg={3} style={styles.leftBar}>
+            <Image src="images/SignMeIn_logo.png" style={styles.image} />
+            <ButtonGroup>
+              {radios.map((radio, idx) => (
+                <Button
+                  key={idx}
+                  id={`radio-${idx}`}
+                  type="radio"
+                  variant="custom"
+                  value={radio.value}
+                  checked={radioValue === radio.value}
+                  onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  onClick={() => {
+                    setLoginForm(radio.login);
+                  }}
+                >
+                  {radio.name}
+                </Button>
+              ))}
+            </ButtonGroup>
+            {loginForm && <LoginForm />}
+            {!loginForm && <SignupForm />}
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 

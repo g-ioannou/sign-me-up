eb922e0c22d9884b1dbf75f63ebfe16d59f4cae4
JoFoto <g-m-f@hotmail.com>
2022-06-01 03:35:24 +0300

    Create model serializers

---
+++ server/api/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from models import User,Admin,SimpleUser,Store,Purchase,Event,Review
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Admin
+        fields = '__all__'
+
+class SimpleUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SimpleUser
+        fields = '__all__'
+
+class StoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Store
+        fields = '__all__'
+
+class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase
+        fields = '__all__'
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
